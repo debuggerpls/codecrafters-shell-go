d8f7ed86845df2e48eafce946c75240f7f33c16c
Deividas Puplauskas <ngivingup@gmail.com>
2025-02-13 22:40:28 +0100

    add echo builtin

--- pkg/shell/shell.go
+++ pkg/shell/shell.go
@@ -5,24 +5,50 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
+const (
+	prompt = "$ "
+)
+
 func RunShell(ctx context.Context, stdin io.Reader, stdout io.Writer) int {
-	fmt.Fprint(stdout, "$ ")
+	fmt.Fprint(stdout, prompt)
 
 	// Wait for user input
 	scanner := bufio.NewScanner(stdin)
 	for scanner.Scan() {
+		// https://www.gnu.org/software/bash/manual/bash.html#Shell-Operation
+		// 1. read input from user's terminal
 		input := scanner.Text()
-		if strings.HasPrefix(input, "exit") {
-			// TODO: return other values?
+
+		// 2. break input into words and operators (tokenize), obey quoting rules
+		tokens := strings.Split(input, " ")
+
+		// 3. parse tokens into simple and compound commands
+		command := tokens[0]
+		args := tokens[1:]
+
+		// 4. perform shell expansions
+
+		// 5. perform redirections
+
+		// 6. execute the command
+		// 7. optionally wait for command to complete and collect its exit status
+		switch command {
+		case "exit":
+			if code, err := strconv.Atoi(args[0]); err == nil {
+				return code
+			}
 			return 0
+		case "echo":
+			fmt.Fprintln(stdout, strings.Join(args, " "))
+		default:
+			fmt.Fprintf(stdout, "%s: command not found\n", command)
 		}
 
-		fmt.Fprintf(stdout, "%s: command not found\n", scanner.Text())
-
-		fmt.Fprint(stdout, "$ ")
+		fmt.Fprint(stdout, prompt)
 	}
 
 	return 1
--- pkg/shell/shell_test.go
+++ pkg/shell/shell_test.go
@@ -107,3 +107,23 @@ func TestExit(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestEcho(t *testing.T) {
+	stdout := bytes.Buffer{}
+	stdin := bytes.NewBufferString("echo hello world\n")
+	ctx, cancel := context.WithCancel(t.Context())
+	t.Cleanup(cancel)
+	go RunShell(ctx, stdin, &stdout)
+	// give some time to start the goroutine
+	time.Sleep(time.Millisecond)
+
+	scanner := bufio.NewScanner(&stdout)
+	if !scanner.Scan() {
+		t.Fatalf("expected echo back")
+	}
+	want := "$ hello world"
+	got := scanner.Text()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
